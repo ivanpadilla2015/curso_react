8ca853565cf5bce74067595ed35566c209f8948a
ivanPadilla <ivanpadillamol@gmail.com>
2022-03-09 17:28:52 -0500

    Commit tercera clase

---
+++ part2/src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom';
+
+
+
+const rootElement = document.getElementById('root');
+
+const App = ()=>{
+  return <h1>Hola clase minibootcam</h1>
+}
+
+ReactDOM.render(<App />, rootElement);
+ 
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
