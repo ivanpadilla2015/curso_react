6236559e21177636dca3e4d620eab73aa76669b6
ivanPadilla <ivanpadillamol@gmail.com>
2022-03-09 17:55:07 -0500

    reder setinterval

--- part2/src/index.js
+++ part2/src/index.js
@@ -4,13 +4,31 @@ import ReactDOM from 'react-dom';
 
 const rootElement = document.getElementById('root');
 
-const App = ()=>{
-  return <h1>Hola clase minibootcam</h1>
+const App = (props)=>{
+  return <h1>{ props.contadorInicia}</h1>
 }
 
-ReactDOM.render(<App />, rootElement);
- 
+let contador = 0;
+ const refresh = () =>{
+  ReactDOM.render(<App contadorInicia = {contador} />, rootElement);
+ }
 
+ setInterval( ()=>{
+  contador++;
+  refresh();
+ }, 1000);
+/*
+let contador = 3;
+
+ReactDOM.render(<App contadorInicia = {contador} />, rootElement);
+ 
+contador++;
+
+ReactDOM.render(<App contadorInicia = {contador} />, rootElement);
+
+contador++;
+
+ReactDOM.render(<App contadorInicia = {contador} />, rootElement);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
